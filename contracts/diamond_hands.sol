608cc3dd737fdc07a62ec505838f66325ebecb3b
Jack Antico <you@example.com>
2022-03-07 12:36:46 -0500

    Flattened code, this is commit 2022-03-03-12:36

--- contracts/diamond_hands.sol
+++ contracts/diamond_hands.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 pragma solidity ^0.8.0;
 
